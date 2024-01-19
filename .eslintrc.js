f9a2c59f0bf4fa4a1a404e0e307af63da0b3a337
Andrey Semenov <semenov.orc@gmail.com>
2024-01-19 14:27:13 +0300

    change .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
   },
-  extends: ["eslint:recommended", "prettier"],
-  overrides: [
+  'extends': 'eslint:recommended',
+  'overrides': [
     {
-      files: ["**/*/*.test.js"],
-      plugins: ["jest"],
-      extends: ["plugin:jest/recommended"],
-      rules: { "jest/prefer-expect-assertions": "off" },
-    },
+      'files': ['**/*/*.test.js'],
+      'plugins': ['jest'],
+      'extends': ['plugin:jest/recommended'],
+      'rules': { 'jest/prefer-expect-assertions': 'off' }
+    }
   ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
   },
-  rules: {
-    "prettier/prettier": "off",
-    quotes: [
-      "warn",
-      {
-        singleQuote: true,
-        semi: true,
-      },
-    ],
-  },
-};
+  'rules': {}
+}
